dfd47ad6d2d157e45825b6f8f551ee2cd4fc30d0
SquirrelEnjoyer23 <robloxplayer9000123@gmail.com>
2026-06-28 22:58:48 -0700

    Added overlay capabilities
    Added the ability to display images

--- Main.py
+++ Main.py
@@ -1,20 +1,32 @@
-# Version 1 of this application
+# Version 3 of this application
 
 import tkinter as Gui
+import ctypes
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 from pathlib import Path
-
+from PIL import Image, ImageTk
 
 MainMenu = Gui.Tk()
+width = MainMenu.winfo_screenwidth()
+height = MainMenu.winfo_screenheight()
 MainMenu.title("Custom Crosshairs")
 MainMenu.geometry("500x600")
 MainMenu.config(bg="#313131")
 
 OverlayMenu = Gui.Toplevel()
+OverlayMenu.overrideredirect(True)
+OverlayMenu.attributes('-topmost', True, '-alpha', 0.5)
 OverlayMenu.title("Overlay")
-OverlayMenu.geometry("200x200")
+
+centerx = int(width / 2 - 200 / 2)
+centery = int(height / 2 - 200 / 2)
+
+OverlayMenu.geometry(f"200x200+{centerx}+{centery}")
+OverlayMenu.withdraw()
+
+OverlayMenu.update()
 
 file_path = ""
 
@@ -36,17 +48,19 @@ def ChoseFile():
 def VerifyFile():
     if file_path:
         print(Path(file_path).name)
+        global ImageDisplay
+        ImageDisplay = Gui.PhotoImage(file=(Path(file_path)))
+        ImageLabel = ttk.Label(OverlayMenu, image=ImageDisplay)
+        ImageLabel.pack()
+        OverlayMenu.deiconify()
 
 ImportButton = ttk.Button(MainMenu,text="Import new picture",command=lambda: ChoseFile(),style="ImportButton.TButton")
 ImportButton.pack(padx=10, pady=150)
 
 UpdateButton = ttk.Button(MainMenu,text="Update",command=lambda: VerifyFile(),style="Button.TButton")
-UpdateButton.pack(side="bottom", anchor="s", padx=10, pady=0)
+UpdateButton.pack(side="bottom", anchor="s", padx=10, pady=0) 
 
 
-ImageDisplay = Gui.PhotoImage(file="Test.png")
-
-ImageLabel = ttk.Label(OverlayMenu, image=ImageDisplay)
-ImageLabel.pack()
-
+print(width)
+print(height)
 MainMenu.mainloop()
